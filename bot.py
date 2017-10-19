7ae9ecba202388e42ae57ab9ab80f9dc522b7d59
amarunenko <sashamarunenko@gmail.com>
2017-10-19 21:30:13 +0300

    'fsdf'

---
+++ bot.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import redis
+import os
+import telebot
+# import some_api_lib
+# import ...
+
+# Example of your code beginning
+#           Config vars
+token = os.environ['43422798:AAHZmAoi3WPoe4F4TQcBykrsK1ZZNTp5qmo']
+some_api_token = os.environ['SOME_API_TOKEN']
+#             ...
+
+# If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
+r = redis.from_url(os.environ.get("REDIS_URL"))
+
+#       Your bot code below
+# bot = telebot.TeleBot(token)
+# some_api = some_api_lib.connect(some_api_token)
+#              ...
